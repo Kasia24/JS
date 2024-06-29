17e6aa4dd1d28a069137ef6d9816f74f9bf9ee7a
Kasia24 <160325853+Kasia24@users.noreply.github.com>
2024-06-29 14:07:19 +0200

    add commit

--- goit-js-hw-01/js/task-1.js
+++ goit-js-hw-01/js/task-1.js
@@ -1,5 +1,4 @@
-<script>
-    function makeTransaction(quantity, pricePerDroid) {
+function makeTransaction(quantity, pricePerDroid) {
     console.log(`You ordered ${quantity} droids worth ${totalPrice} credits!`);
     totalPrice(quantity * pricePerDroid);
 }
@@ -7,4 +6,3 @@
 console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
 console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
 console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
-</script>
