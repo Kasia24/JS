8214f16fb5836944129f59d39a9218fc0693c605
Kasia24 <160325853+Kasia24@users.noreply.github.com>
2024-07-01 19:59:25 +0200

    add commit

--- goit-js-hw-01/js/task-3.js
+++ goit-js-hw-01/js/task-3.js
@@ -1,11 +1,11 @@
-// function getElementWidth(content, padding, border) {
-const a = content.replace("px", ""),
-    b = padding.split("px")[0],
-    c = border.slice(0, -2);
-console.log(a, b, c);
-return Number(a) + 2 * Number(b) + 2 * Number(c);
-// }
+function getElementWidth(content, padding, border) {
+    const a = content.replace("px", ""),
+        b = padding.split("px")[0],
+        c = border.slice(0, -2);
+    console.log(a, b, c);
+    return Number(a) + 2 * Number(b) + 2 * Number(c);
+}
 
-// console.log(getElementWidth("50px", "8px", "4px")); // 74
-// console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-// console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
