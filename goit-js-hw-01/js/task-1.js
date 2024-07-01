04211b2ae8d9c0db203b8494d04ccf361636c3c1
Kasia24 <160325853+Kasia24@users.noreply.github.com>
2024-07-01 19:55:40 +0200

    add commit

--- goit-js-hw-01/js/task-1.js
+++ goit-js-hw-01/js/task-1.js
@@ -1,21 +1,21 @@
-// function makeTransaction(quantity, pricePerDroid) {
-// const totalPrice = quantity * pricePerDroid;
-// return totalPrice;
-// }
+function makeTransaction(quantity, pricePerDroid) {
+    const totalPrice = quantity * pricePerDroid;
+    return totalPrice;
+}
 
-// const quantity = 5;
-// const pricePerDroid = 3000;
+const quantity = 5;
+const pricePerDroid = 3000;
 
-// const totalPrice = makeTransaction(quantity, pricePerDroid);
-// console.log(`You ordered ${quantity} droids worth ${totalPrice} credits!`);
+const totalPrice = makeTransaction(quantity, pricePerDroid);
+console.log(`You ordered ${quantity} droids worth ${totalPrice} credits!`);
 
-// function makeTransaction(quantity, pricePerDroid) {
-// const totalPrice = quantity * pricePerDroid;
-// return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-// }
-// const message = makeTransaction(5, 3000);
-// console.log(message); // "You ordered 5 droids worth 15000 credits!"
+function makeTransaction(quantity, pricePerDroid) {
+    const totalPrice = quantity * pricePerDroid;
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+}
+const message = makeTransaction(5, 3000);
+console.log(message); // "You ordered 5 droids worth 15000 credits!"
 
-// console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-// console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-// console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
+console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
